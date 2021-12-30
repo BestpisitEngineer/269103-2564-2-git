5e3e20b971ebfb7313118244d5cddcbaf5e33d61
BestpisitEngineer <93572773+BestpisitEngineer@users.noreply.github.com>
2021-12-30 15:08:34 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#Set the variable OUTPUT to the file in the directory ~/ follow by the file that has a name 'backup_' to the current date in date+%Y%m%d format
 OUTPUT=~/backup_$(date +%Y%m%d).tgz
+#create new archive and perform operation verbosely by using gzip operation algorithm and specify the file name to $OUTPUT file that added in argument#1 directory
 tar -cvzf $OUTPUT $1
